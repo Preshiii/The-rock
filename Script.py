c5eefd3fac81426bf4719d7a0e7160b905098b6a
Preshiii <94832799+Preshiii@users.noreply.github.com>
2022-02-20 13:13:09 +0100

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,7 +3,7 @@ class script(object):
 
 SWEETIE MY NAME IS  <a href=https://t.me/{}>{}</a>, I CAN PROVIDE YOU MOVIES, JUST SEND ME ONLY THE NAME OF THE MOVIE AND WATCH ME DO MY MAGIC!!! 
 
-✯ FOR MORE HELP ON HOW TO USE ME - https://t.me/Movie_Collections_Bot/1410?single"""
+✯ FOR MORE HELP ON HOW TO USE ME - https://t.me/Movie_Collections_Bot/1589"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
